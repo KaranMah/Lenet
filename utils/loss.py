d7bb27639154cbdf2799eb81a5d9822cfeac0fa2
Karan M <karan22.mahajan@gmail.com>
2020-05-04 02:28:39 +0530

    removed debug prints

--- utils/loss.py
+++ utils/loss.py
@@ -1,11 +1,14 @@
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-# loss
 def cross_entropy(pred, Y):
-    # Y = Y.reshape(-1, 1)
+    temp =  np.argmax(pred, axis=1)
+    ctr = 0
+    for i in range(len(temp)):
+        if temp[i] == Y[i]:
+            ctr +=1
     Y = np.eye(10)[Y]
     assert Y.shape == pred.shape
 
     epsilon = 1e-10
     loss = (-Y * np.log(pred + epsilon)).sum() / pred.shape[0]
+    print (loss, ctr)
     return loss
