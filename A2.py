d1da374185b3870d5bd161c621091343412d0957
Karan M <karan22.mahajan@gmail.com>
2020-05-04 21:56:13 +0530

    reduce test

--- A2.py
+++ A2.py
@@ -133,8 +133,17 @@ def train(model, train_data, test_data, num_epoch, lr_global_list, batch_size):
             model.Back_Propagation(lr_global)
         print("Done, total cost of epoch {}: {}".format(epoch + 1, cost))
         # For your implementation
-        error01_train, _ = model.Forward_Propagation(train_data[0], train_data[1], 'test')
-        error01_test, _ = model.Forward_Propagation(test_data[0], test_data[1], 'test')
+        error01_train = 0
+        error01_test = 0
+        for i in range(6):
+            error1_train, _ = model.Forward_Propagation(
+                train_data[0][i*len(train_data[0]):(i+1)*len(train_data[0])],
+                train_data[1][i*len(train_data[0]):(i+1)*len(train_data[0])], 'test')
+            error1_test, _ = model.Forward_Propagation(
+                train_data[0][i * len(test_data[0]):(i + 1) * len(test_data[0])],
+                train_data[1][i * len(test_data[0]):(i + 1) * len(test_data[0])], 'test')
+            error01_train += error1_train
+            error01_test += error1_test
         err_rate_list.append([error01_train/60000, error01_test/10000])
         print("0/1 error of testing set: ", error01_test, "/", len(test_data[1]))
         print("Time used: ", time.time() - ste, "sec")
